18032c415d3aa32cc0007e0055ca69b0891f6b32
nabilchabab <nabil.chababnabil@gmail.com>
2023-11-17 22:29:18 +0100

    Add Joins

--- script.sql
+++ script.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE Bookify;
 
-CREATE TABLE Équipements (
-    Id INT PRIMARY KEY,
+CREATE TABLE Equipements (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
     Nom VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Salles (
-    Id INT PRIMARY KEY,
+    Id INT PRIMARY KEY AUTO_INCREMENT,
     Nom VARCHAR(255) NOT NULL,
     Capacité VARCHAR(255) NOT NULL,
     equipements_Id INT,
@@ -14,12 +14,12 @@ CREATE TABLE Salles (
 );
 
 CREATE TABLE Departement (
-    Id INT PRIMARY KEY,
+    Id INT PRIMARY KEY AUTO_INCREMENT,
     Poste VARCHAR(255) NOT NULL,
 );
 
-CREATE TABLE Employés (
-    Id INT PRIMARY KEY,
+CREATE TABLE Employes (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
     Nom VARCHAR(255) NOT NULL,
     Email VARCHAR(255) NOT NULL,
     Département_Id INT,
@@ -27,8 +27,8 @@ CREATE TABLE Employés (
 );
 
 
-CREATE TABLE Réservations (
-    Id INT PRIMARY KEY,
+CREATE TABLE Reservations (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
     Salle_Id INT,
     Employé_Id INT,
     Date_Debut DATE,
@@ -46,21 +46,45 @@ INSERT INTO Équipements (Nom) VALUES ('Les Ordinateurs');
 INSERT INTO Salles (Nom, Capacité, equipements_Id) VALUES ('Salle 1', '23', 1);
 INSERT INTO Salles (Nom, Capacité, equipements_Id) VALUES ('Salle 2', '23', 2);
 INSERT INTO Salles (Nom, Capacité, equipements_Id) VALUES ('Salle 3', '25', 3);
+INSERT INTO Salles (Nom, Capacité, equipements_Id) VALUES ('Salle 4', '25', 4);
 
 INSERT INTO Departement (Poste) VALUES ('DEV MOBILE');
 INSERT INTO Departement (Poste) VALUES ('DEV WEB');
 INSERT INTO Departement (Poste) VALUES ('DEVOPS');
+INSERT INTO Departement (Poste) VALUES ('BIG DATA');
 
 INSERT INTO Employés (Nom, Email, Département_Id) VALUES ('Said Aabilla', 'said.aabilla@gmail.com', 1);
 INSERT INTO Employés (Nom, Email, Département_Id) VALUES ('Nabil Chabab', 'nabil.chababnabil@gmail.com', 2);
 INSERT INTO Employés (Nom, Email, Département_Id) VALUES ('Zakaria Elkoh ', 'zakaria.elkoh@gmail.com', 3);
+INSERT INTO Employés (Nom, Email, Département_Id) VALUES ('Adnane Roujane ', 'adnane.roujane@gmail.com', 4);
 
-INSERT INTO Réservations (Salle_Id, Employé_Id, Date_Debut, Date_Fin) VALUES (1, 1, '16-11-2023', '17-11-2023');
-INSERT INTO Réservations (Salle_Id, Employé_Id, Date_Debut, Date_Fin) VALUES (2, 2, '17-11-2023', '18-11-2023');
-INSERT INTO Réservations (Salle_Id, Employé_Id, Date_Debut, Date_Fin) VALUES (3, 3, '18-11-2023', '19-11-2023');
+INSERT INTO Réservations (Salle_Id, Employé_Id, Date_Debut, Date_Fin) VALUES (1, 1, '2023-11-16', '2023-11-17');
+INSERT INTO Réservations (Salle_Id, Employé_Id, Date_Debut, Date_Fin) VALUES (2, 2, '2023-11-17', '2023-11-18');
+INSERT INTO Réservations (Salle_Id, Employé_Id, Date_Debut, Date_Fin) VALUES (3, 3, '2023-11-18', '2023-11-19');
+INSERT INTO Réservations (Salle_Id, Employé_Id, Date_Debut, Date_Fin) VALUES (3, 3, '2023-11-19', '2023-11-20');
 
 --List (SELECT)
 
 SELECT * FROM Employés;
 SELECT * FROM Employés WHERE Id = 1;
 SELECT * FROM Employés WHERE Nom LIKE 'N%';
+
+
+SELECT Réservations.*, Salles.Nom AS Nom_Salle
+FROM Réservations
+JOIN Employés ON Réservations.Employé_Id = Employés.Id
+JOIN Salles ON Réservations.Salle_Id = Salles.Id
+WHERE Employés.Nom = 'Nabil Chabab';
+
+SELECT Reservations.*, Salles.Nom AS Nom_Salle, Employes.Nom AS Nom_Employe
+FROM Reservations
+JOIN Employes ON Reservations.Employé_Id = Employes.Id
+JOIN Salles ON Reservations.Salle_Id = Salles.Id
+JOIN Departement ON Employes.Département_Id = Departement.Id
+WHERE Departement.Poste = 'DEV MOBILE';
+
+SELECT Reservations.*, Salles.Nom AS Nom_Salle, Employes.Nom AS Nom_Employe
+FROM Reservations
+JOIN Employes ON Reservations.Employé_Id = Employes.Id
+JOIN Salles ON Reservations.Salle_Id = Salles.Id
+WHERE Salles.Nom = 'Salle 1';
